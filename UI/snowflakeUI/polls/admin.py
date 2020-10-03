d4075caafd5c9c3982907eac41e7589f24f3e983
Zhi Zhou <alan.zhizhou@gmail.com>
2020-10-02 23:27:56 -0700

    Customize app and admin site

--- UI/snowflakeUI/polls/admin.py
+++ UI/snowflakeUI/polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Query', {'fields': ['question_text']}),
+        ('Results', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+admin.site.register(Question, QuestionAdmin)
